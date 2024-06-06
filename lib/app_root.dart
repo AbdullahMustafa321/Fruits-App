e65348f45a9e9856a43bb2b1b548cc5df0825c64
AbdullahMustafa321 <mostafaabdala505@gmail.com>
2024-06-06 18:12:57 +0300

    Create HomeView

--- lib/app_root.dart
+++ lib/app_root.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'features/home/presentation/views/home_view.dart';
 
 class AppRoot extends StatelessWidget {
   const AppRoot({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      designSize: const Size(360,690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_,context){
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomeView()
+          ,
+        );
+      },
+    );
   }
 }
